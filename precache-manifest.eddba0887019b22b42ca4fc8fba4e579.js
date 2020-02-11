8b0aea7e3d0991103e7397058526f77758b7e005
Chris Cuomo <weatherfields@protonmail.com>
2020-02-10 20:12:15 -0500

    Updates

---
+++ precache-manifest.eddba0887019b22b42ca4fc8fba4e579.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c07fa5b0c3685200aa7f49f9dae3dd1",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "e6f199e59914d65d565b",
+    "url": "/react-memory-game/static/css/main.ddb62c9d.chunk.css"
+  },
+  {
+    "revision": "d86f7002620544aa76ba",
+    "url": "/react-memory-game/static/js/2.20460e59.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-memory-game/static/js/2.20460e59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f199e59914d65d565b",
+    "url": "/react-memory-game/static/js/main.c737a922.chunk.js"
+  },
+  {
+    "revision": "48c15ff71f3b4493c02c",
+    "url": "/react-memory-game/static/js/runtime-main.9ed46399.js"
+  },
+  {
+    "revision": "ddccdd0b6deb1617aad47ae983e646e6",
+    "url": "/react-memory-game/static/media/MustangR500.ddccdd0b.png"
+  },
+  {
+    "revision": "5394e488427b5304a64dab2ad03cc0f7",
+    "url": "/react-memory-game/static/media/StayWriter-Handmade.5394e488.otf"
+  },
+  {
+    "revision": "f00c777074b2b13fdf7a055b825a0df1",
+    "url": "/react-memory-game/static/media/navGtr.f00c7770.png"
+  }
+]);
